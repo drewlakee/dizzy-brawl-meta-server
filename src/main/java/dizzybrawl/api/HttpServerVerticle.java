ea8652777faff605668b7a729cbe5f060e5b7b55
Andrew Aleynikov <drew.lake@yandex.ru>
2020-10-05 14:13:12 +0300

    test http get, db query by id

--- src/main/java/dizzybrawl/api/HttpServerVerticle.java
+++ src/main/java/dizzybrawl/api/HttpServerVerticle.java
@@ -2,50 +2,50 @@ package dizzybrawl.api;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.DeliveryOptions;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.core.json.Json;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 public class HttpServerVerticle extends AbstractVerticle {
     private static final String CONFIG_HTTP_SERVER_PORT = "http.server.port";
+    public static final String CONFIG_DIZZYBRAWL_DB_QUEUE = "dizzybrawl.db.queue";
 
     private static final Logger log = LoggerFactory.getLogger(HttpServerVerticle.class);
 
+    private String dizzyBrawlDbQueue = "dizzybrawl.db.queue";
+
     @Override
     public void start(Promise<Void> startPromise) {
-        Router router = buildRouter();
-        vertx
-                .createHttpServer()
-                .requestHandler(router)
-                .listen(8080, "localhost", asyncResult -> {
-                    if (asyncResult.succeeded()) {
-                        log.info("HTTP server running on port " + 8080);
-                        startPromise.complete();
-                    } else {
-                        log.error("Could not start a HTTP server.", asyncResult.cause());
-                        startPromise.fail(asyncResult.cause());
-                    }
-                });
-    }
-
-    private Router buildRouter() {
         Router router = Router.router(vertx);
         router.mountSubRouter(EndPoints.MOUNT_SUB_API, router);
 
-        router.get(EndPoints.GET_HERO_BY_ID).handler(this::getHero);
+        // handlers
+        router.get(EndPoints.GET_HERO_BY_ID).handler(this::getHeroById);
 
-        return router;
+        vertx.createHttpServer()
+             .requestHandler(router)
+             .listen(config().getInteger(CONFIG_HTTP_SERVER_PORT, 8080));
     }
 
-    private void getHero(RoutingContext context) {
-        String id = context.request().getParam("id");
+    private void getHeroById(RoutingContext context) {
+        // request to event bus
+        String requestedHeroId = context.request().getParam("id");
+        JsonObject request = new JsonObject().put("hero", requestedHeroId);
 
-        context
-                .request()
-                .response()
-                .putHeader("content-type", "application/json")
-                .end(Json.encodePrettily(id));
+        // options for event bus
+        DeliveryOptions options = new DeliveryOptions().addHeader("action", "get-hero-by-id");
+
+        // async handler for received response from event bus
+        vertx.eventBus().request(dizzyBrawlDbQueue, request, options, reply -> {
+           if (reply.succeeded()) {
+               JsonObject responseBody = (JsonObject) reply.result().body();
+               context.end(responseBody.encodePrettily());
+           } else {
+               context.fail(reply.cause());
+           }
+        });
     }
 }
--- src/main/java/dizzybrawl/database/DatabaseVerticle.java
+++ src/main/java/dizzybrawl/database/DatabaseVerticle.java
@@ -2,11 +2,19 @@ package dizzybrawl.database;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.eventbus.Message;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
-import io.vertx.sqlclient.PoolOptions;
+import io.vertx.sqlclient.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Properties;
 
 
 public class DatabaseVerticle extends AbstractVerticle {
@@ -17,12 +25,31 @@ public class DatabaseVerticle extends AbstractVerticle {
     private static final String CONFIG_PG_PASSWORD = "postgresql.password";
     private static final String CONFIG_PG_POOL_MAX_SIZE = "postgresql.pool.maxsize";
 
+    // Event bus address, that verticle will listen
+    public static final String CONFIG_DIZZYBRAWL_DB_QUEUE = "dizzybrawl.db.queue";
+
     private static final Logger log = LoggerFactory.getLogger(DatabaseVerticle.class);
 
     private PgPool pgPool;
 
+    private final HashMap<SqlQuery, String> sqlQueries = new HashMap<>();
+
+    private enum SqlQuery {
+        GET_HERO_BY_ID
+    }
+
+    public enum ErrorCodes {
+        NO_ACTION_SPECIFIED,
+        BAD_ACTION,
+        DB_ERROR
+    }
+
     @Override
-    public void start(Promise<Void> startPromise) {
+    public void start(Promise<Void> startPromise) throws IOException {
+        // Block Loop Event, but start() calls only at application launch
+        loadSqlQueries();
+
+        // Configure connection to database
         PgConnectOptions connectOptions = new PgConnectOptions()
                 .setHost(config().getString(CONFIG_PG_HOST, "localhost"))
                 .setPort(config().getInteger(CONFIG_PG_PORT, 5432))
@@ -32,13 +59,78 @@ public class DatabaseVerticle extends AbstractVerticle {
 
         // Workers pool options
         PoolOptions poolOptions = new PoolOptions()
-                .setMaxSize(config().getInteger(CONFIG_PG_POOL_MAX_SIZE, 20));
+                .setMaxSize(config().getInteger(CONFIG_PG_POOL_MAX_SIZE, 30));
 
         // Create the client pool
         pgPool = PgPool.pool(vertx, connectOptions, poolOptions);
 
-        // TODO: Insert promise to services init
-        startPromise.complete();
+        // TODO: Add tables creation
+        pgPool.getConnection(asyncResult -> {
+            if (asyncResult.succeeded()) {
+                vertx.eventBus().consumer(CONFIG_DIZZYBRAWL_DB_QUEUE, this::onMessageReceive);
+                startPromise.complete();
+            } else {
+                startPromise.fail(asyncResult.cause());
+            }
+        });
+    }
+
+    private void loadSqlQueries() throws IOException {
+        InputStream queriesInputStream = getClass().getResourceAsStream("/db-queries.properties");
+
+        Properties queriesProps = new Properties();
+        queriesProps.load(queriesInputStream);
+        queriesInputStream.close();
+
+        sqlQueries.put(SqlQuery.GET_HERO_BY_ID, queriesProps.getProperty("get-hero-by-id"));
+    }
+
+    public void onMessageReceive(Message<JsonObject> message) {
+        if (!message.headers().contains("action")) {
+            message.fail(ErrorCodes.NO_ACTION_SPECIFIED.ordinal(), "No action header specified");
+            return;
+        }
+
+        String action = message.headers().get("action");
+
+        switch (action) {
+            case "get-hero-by-id":
+                getHeroById(message);
+                break;
+            default:
+                message.fail(ErrorCodes.BAD_ACTION.ordinal(), "Bad action: " + action);
+        }
+    }
+
+    private void getHeroById(Message<JsonObject> message) {
+        // body of request on event bus
+        Integer requestedHeroId = Integer.parseInt(message.body().getString("hero"));
+        JsonObject response = new JsonObject();
+
+        pgPool.getConnection(ar1 -> {
+            if (ar1.succeeded()) {
+                SqlConnection connection = ar1.result();
+                connection
+                        .preparedQuery(sqlQueries.get(SqlQuery.GET_HERO_BY_ID))
+                        .execute(Tuple.of(requestedHeroId), ar2 -> {
+                            RowSet<Row> resultQuery = ar2.result();
+
+                            if (resultQuery.iterator().hasNext()) {
+                                Row heroRow = resultQuery.iterator().next();
+                                response.put("hero_id", heroRow.getInteger("hero_id"));
+                                response.put("name", heroRow.getString("name"));
+                                response.put("level", heroRow.getInteger("level"));
+                            } else {
+                                response.put("message", "not found");
+                            }
+
+                            message.reply(response);
+                        });
+            } else {
+                log.error("Could not connect: " + ar1.cause().getMessage());
+                response.put("message", "could not connect " + ar1.cause().getMessage());
+            }
+        });
     }
 
     @Override
