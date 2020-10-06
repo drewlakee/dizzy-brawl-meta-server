86050272926f9a4affd1811a18ef543922255af7
Andrew Aleynikov <drew.lake@yandex.ru>
2020-10-06 11:06:46 +0300

    add proxie service impl

--- src/main/java/dizzybrawl/database/services/impls/PgHeroService.java
+++ src/main/java/dizzybrawl/database/services/impls/PgHeroService.java
@@ -3,16 +3,36 @@ package dizzybrawl.database.services.impls;
 import dizzybrawl.database.services.HeroService;
 import dizzybrawl.database.sql.SqlQuery;
 import io.vertx.core.AsyncResult;
+import io.vertx.core.Future;
 import io.vertx.core.Handler;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.logging.Logger;
+import io.vertx.core.logging.LoggerFactory;
+import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.SqlClient;
 
 import java.util.HashMap;
 
 public class PgHeroService implements HeroService {
 
-    public PgHeroService(SqlClient dbClient, HashMap<SqlQuery, String> sqlQueries, Handler<AsyncResult<HeroService>> readyHandler) {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PgHeroService.class);
 
+    private final HashMap<SqlQuery, String> sqlQueries;
+    private final PgPool dbClient;
+
+    public PgHeroService(SqlClient dbClient, HashMap<SqlQuery, String> sqlQueries, Handler<AsyncResult<HeroService>> readyHandler) {
+        this.dbClient = (PgPool) dbClient;
+        this.sqlQueries = sqlQueries;
+
+        this.dbClient.getConnection(ar -> {
+            if (ar.failed()) {
+                LOGGER.error("Could not open a database connection", ar.cause());
+                readyHandler.handle(Future.failedFuture(ar.cause()));
+            } else {
+                // TODO: CREATE TABLES
+                readyHandler.handle(Future.succeededFuture(this));
+            }
+        });
     }
 
     @Override
