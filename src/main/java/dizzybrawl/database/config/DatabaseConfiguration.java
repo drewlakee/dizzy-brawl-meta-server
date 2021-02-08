8359e4b55a49c1723bd1fa0fe4577dbd1d8a24fc
Andrew Aleynikov <drew.lake@yandex.ru>
2021-02-08 21:34:47 +0300

    add prometheus and grafana for web-server monitoring, update configurations

--- src/main/java/dizzybrawl/database/config/DatabaseConfiguration.java
+++ src/main/java/dizzybrawl/database/config/DatabaseConfiguration.java
@@ -4,6 +4,8 @@ import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgConnectOptions;
 import io.vertx.pgclient.PgPool;
 import io.vertx.sqlclient.PoolOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,8 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class DatabaseConfiguration {
 
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
+
     private final Environment environment;
 
     @Autowired
@@ -28,19 +32,19 @@ public class DatabaseConfiguration {
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("hibernate.hbm2ddl.auto"));
+        properties.put("hibernate.dialect", environment.getProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect"));
+        properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql", "false"));
+        properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto", "create"));
         return properties;
     }
 
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getProperty("database.jdbc.driverClassName"));
-        dataSource.setUrl(environment.getProperty("database.url"));
-        dataSource.setUsername(environment.getProperty("database.username"));
-        dataSource.setPassword(environment.getProperty("database.password"));
+        dataSource.setDriverClassName(environment.getProperty("database.jdbc.driverClassName", "org.postgresql.Driver"));
+        dataSource.setUrl(environment.getProperty("database.url", "jdbc:postgresql://postgres/dizzy-brawl"));
+        dataSource.setUsername(environment.getProperty("database.username", "dizzy-brawl"));
+        dataSource.setPassword(environment.getProperty("database.password", "dizzy-brawl"));
         return dataSource;
     }
 
@@ -56,24 +60,18 @@ public class DatabaseConfiguration {
     @Bean
     public PgPool getReactiveVertxPgPool() {
         PgConnectOptions connectionOptions = new PgConnectOptions()
-                .setHost(environment.getProperty("database.host"))
-                .setDatabase(environment.getProperty("database.name"))
-                .setUser(environment.getProperty("database.username"))
-                .setPassword(environment.getProperty("database.password"));
+                .setHost(environment.getProperty("database.host", "postgres"))
+                .setDatabase(environment.getProperty("database.name", "dizzy-brawl"))
+                .setUser(environment.getProperty("database.username", "dizzy-brawl"))
+                .setPassword(environment.getProperty("database.password", "dizzy-brawl"));
 
-        // maybe build at docker then port is not mandatory
-        if (environment.containsProperty("database.port") && !environment.getProperty("database.port").equals("none")) {
-            connectionOptions.setPort(environment.getProperty("database.port", Integer.class));
+        if (environment.containsProperty("database.port")) {
+            connectionOptions.setPort(environment.getRequiredProperty("database.port", Integer.class));
         }
 
         PoolOptions connectionPoolOptions = new PoolOptions();
-
-        if (environment.containsProperty("database.connection.pool.count")
-                && environment.getProperty("database.connection.pool.count", Integer.class) > 0) {
-            connectionPoolOptions.setMaxSize(environment.getProperty("database.connection.pool.count", Integer.class));
-        } else {
-            connectionPoolOptions.setMaxSize(1);
-        }
+        connectionPoolOptions.setMaxSize(environment.getProperty("database.connection.pool.count", Integer.class, 1));
+        log.info("Database connections pool size: {}", environment.getProperty("database.connection.pool.count", Integer.class, 1));
 
         return PgPool.pool(Vertx.vertx(), connectionOptions, connectionPoolOptions);
     }
